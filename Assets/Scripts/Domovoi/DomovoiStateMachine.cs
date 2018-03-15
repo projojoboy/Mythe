62d85bbf2cf3add8e731843249ed25cfa414fd2f
ccfc <cecdc>
2018-03-15 12:57:23 +0100

    Domovoi StateMachine V1

---
+++ Assets/Scripts/Domovoi/DomovoiStateMachine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DomovoiStateMachine : MonoBehaviour {
+
+    private enum _eStates { Walk, Idle }
+    private _eStates _state = _eStates.Walk;
+	
+	void Update () {
+        StateMachine();
+	}
+
+    private void StateMachine()
+    {
+        switch (_state)
+        {
+            case _eStates.Walk:
+
+                break;
+            case _eStates.Idle:
+
+                break;
+        }
+    }
+}
